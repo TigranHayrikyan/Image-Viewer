e824a9d599fddb2df11d643dd2ea6ee1fd6f021d
TigranHayrikyan <tigran.hayrikyan@gmail.com>
2023-01-26 14:07:26 +0400

    This is a last version of simple Image Viewer

--- renderer.js
+++ renderer.js
@@ -5,3 +5,12 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+
+var loadFile = function(event) {
+    var image = document.getElementById('image');
+	image.src = URL.createObjectURL(event.target.files[0]);
+	fileName = event.target.files[0].name;
+	document.getElementById("description").style.visibility = "hidden";
+	document.getElementById("parag").textContent = '"' + fileName + '"' + " File is opened";
+};
+
